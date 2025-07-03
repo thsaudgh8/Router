70cf62d26bd39d56f7ae49b874ff14992c508647
thsaudgh8 <thsaudgh8@naver.com>
2025-07-03 14:19:29 +0900

    폼 상태 동기화2

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -3,6 +3,7 @@ import {
   redirect, useNavigation,
 } from "react-router-dom";
 import { getContacts, createContact } from "../contacts";
+import { useEffect } from "react";
 
 export async function action() {
   const contact = await createContact();
@@ -20,6 +21,10 @@ export async function loader({ request }) {
 export default function Root() {
   const { contacts, q } = useLoaderData();
   const navigation = useNavigation();
+  
+  useEffect(() => {
+    document.getElementById("q").value = q;
+  }, [q]);
 
   return (
     <>
