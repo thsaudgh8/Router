48dd28d6bf1ffcdb5a408e79a60b5cb74274fe89
thsaudgh8 <thsaudgh8@naver.com>
2025-07-03 15:39:03 +0900

    연락처 찾을 수 없을 시 오류처리

--- src/routes/contact.jsx
+++ src/routes/contact.jsx
@@ -9,7 +9,13 @@ export async function action({ request, params }) {
 }
 
 export async function loader({ params }) {
-  const contact = await getContact(params.contactId);
+   const contact = await getContact(params.contactId);
+  if (!contact) {
+    throw new Response("", {
+      status: 404,
+      statusText: "Not Found",
+    });
+  }
   return { contact };
 }
 
@@ -80,7 +86,9 @@ export default function Contact() {
 
 function Favorite({ contact }) {
   const fetcher = useFetcher();
-  const favorite = contact.favorite;
+  const favorite = fetcher.formData
+    ? fetcher.formData.get("favorite") === "true"
+    : contact.favorite;
   return (
     <fetcher.Form method="post">
       <button
