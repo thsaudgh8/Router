c0301b543353ba756a084356a90e575758347e22
thsaudgh8 <thsaudgh8@naver.com>
2025-07-03 10:31:58 +0900

    편집기능 추가

--- src/routes/contact.jsx
+++ src/routes/contact.jsx
@@ -8,15 +8,7 @@ export async function loader({ params }) {
 
 export default function Contact() {
   const { contact } = useLoaderData();
-  contact = {
-    first: "Your",
-    last: "Name",
-    avatar: "https://robohash.org/you.png?size=200x200",
-    twitter: "your_handle",
-    notes: "Some notes",
-    favorite: true,
-  };
-
+  
   return (
     <div id="contact">
       <div>
